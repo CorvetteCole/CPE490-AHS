b0be2786cbce8ebf4cf9f1115a9d4408781ebcc4
Cole Gerdemann <corvettecole@gmail.com>
2023-03-20 01:15:36 -0400

    OOP version works

--- comparison_matrix.py
+++ comparison_matrix.py
@@ -1,41 +1,101 @@
 import numpy
+import copy
 from typing import List
 
 random_index = [0, 0, 0.52, 0.89, 1.11, 1.25, 1.35, 1.4, 1.45, 1.49, 1.52]
 
 
+def normalize_geometric(matrix) -> numpy.ndarray:
+    """
+    Compute normalized weights based on Geometric Mean method
+
+    :return: the normalized weight vector
+    """
+    column_sum = numpy.tile(numpy.sum(matrix, 0), [matrix.shape[0], 1])
+    normalized_matrix = numpy.divide(matrix, column_sum)
+    geometric_weight = numpy.prod(normalized_matrix, 1) ** (1.0 / matrix.shape[1])
+    return geometric_weight / numpy.sum(geometric_weight)
+
+
+def normalize_eigen(matrix) -> numpy.ndarray:
+    """
+    Compute normalized weights based on Eigen method
+
+    :return: the normalized weight vector
+    """
+    eigenvalues, normalized_eigenvectors = numpy.linalg.eig(matrix)
+    # get max eigenvalue and corresponding eigenvector
+    max_eigenvalue_index = numpy.argmax(eigenvalues)
+    # where max_eigenvector is the eigenvector corresponding to the maximum eigenvalue
+    max_eigenvector = normalized_eigenvectors[:, max_eigenvalue_index]
+    return max_eigenvector / numpy.sum(max_eigenvector)
+
+
+def calculate_consistency(matrix, normalized_eigen):
+    max_eigenvalue = numpy.max(numpy.linalg.eig(matrix)[0])
+    consistency_index = (max_eigenvalue - matrix.shape[0]) / (matrix.shape[0] - 1)
+    consistency_ratio = consistency_index / random_index[normalized_eigen.shape[0]]
+    return consistency_index, consistency_ratio
+
+
 class ComparisonMatrix:
     # list of self
-    subcriteria: List['ComparisonMatrix']
+    _subcriteria: List['ComparisonMatrix'] = []
+    _matrix: numpy.ndarray
 
-    def __init__(self, matrix, subcriteria=None):
+    _normalized_geometric: numpy.ndarray
+    _normalized_eigen: numpy.ndarray
+    _consistency_index: float
+    _consistency_ratio: float
+
+    def __init__(self, name: str, matrix: numpy.ndarray, subcriteria: List['ComparisonMatrix'] = None):
+        self.name = name
         self.matrix = matrix
-        if subcriteria is None:
-            self._subcriteria = []
-        else:
-            self.subcriteria = subcriteria
+        self.subcriteria = subcriteria
 
     @property
-    def subcriteria(self):
-        return self._subcriteria
+    def subcriteria(self) -> List['ComparisonMatrix']:
+        """ :return: a copy of the subcriteria list, so that it can't be modified """
+        return copy.deepcopy(self._subcriteria)
 
     @subcriteria.setter
-    def subcriteria(self, value):
-        # validate, then recalculate all consistency, ranking, weights
-        pass
+    def subcriteria(self, value: List['ComparisonMatrix']):
+        """
+        :param value: a list of ComparisonMatrix
+        :raises: ValueError if value is not a list of ComparisonMatrix
+        """
+        # validate, ensure that this is a list of ComparisonMatrix
+
+        if value is None:
+            self._subcriteria = []
+            return
+        elif isinstance(value, list):
+            for v in value:
+                if not isinstance(v, ComparisonMatrix):
+                    raise ValueError('subcriteria must be a list of ComparisonMatrix')
+        else:
+            raise ValueError('subcriteria must be a list of ComparisonMatrix')
+
+        self._subcriteria = value
+        self._recalculate_weights()
 
     @property
-    def matrix(self):
-        return self._matrix
+    def matrix(self) -> numpy.ndarray:
+        """ :return: a copy of the matrix, so that it can't be modified """
+        return copy.deepcopy(self._matrix)
 
     @matrix.setter
-    def matrix(self, value):
-        # validate, then recalculate all consistency, ranking, weights
+    def matrix(self, value: numpy.ndarray):
+        """
+        :param value: a numpy.ndarray
+        :raises: ValueError if value is not a numpy.ndarray
+        """
+
+        if not isinstance(value, numpy.ndarray):
+            raise ValueError('matrix must be a numpy.ndarray')
+
         self._matrix = value
-        pass
-
-
-
+        self._recalculate_weights()
 
     @property
     def has_subcriteria(self):
@@ -45,53 +105,51 @@ class ComparisonMatrix:
     def ranking_geometric(self):
         if self.has_subcriteria:
             return numpy.matmul(numpy.stack([s.ranking_geometric for s in self.subcriteria], axis=-1),
-                                self.normalized_geometric)
+                                self._normalized_geometric)
         else:
-            return self.normalized_geometric
+            return self._normalized_geometric
 
     @property
     def ranking_eigen(self):
         if self.has_subcriteria:
             return numpy.matmul(numpy.stack([s.ranking_eigen for s in self.subcriteria], axis=-1),
-                                self.normalized_eigen)
+                                self._normalized_eigen)
         else:
-            return self.normalized_eigen
+            return self._normalized_eigen
 
-    
-    @property
-    def normalized_geometric(self) -> numpy.ndarray:
-        """
-        Compute normalized weights based on Geometric Mean method
-
-        :return: the normalized weight vector
-        """
-        column_sum = numpy.tile(numpy.sum(self.matrix, 0), [self.matrix.shape[0], 1])
-        normalized_matrix = numpy.divide(self.matrix, column_sum)
-        geometric_weight = numpy.prod(normalized_matrix, 1) ** (1.0 / self.matrix.shape[1])
-        return geometric_weight / numpy.sum(geometric_weight)
-
-    @property
-    def normalized_eigen(self) -> numpy.ndarray:
-        """
-        Compute normalized weights based on Eigen method
-
-        :return: the normalized weight vector
-        """
-        eigenvalues, normalized_eigenvectors = numpy.linalg.eig(self.matrix)
-        # get max eigenvalue and corresponding eigenvector
-        max_eigenvalue_index = numpy.argmax(eigenvalues)
-        # where max_eigenvector is the eigenvector corresponding to the maximum eigenvalue
-        max_eigenvector = normalized_eigenvectors[:, max_eigenvalue_index]
-        return max_eigenvector / numpy.sum(max_eigenvector)
-
-    @property
-    def _max_eigenvalue(self) -> float:
-        return numpy.max(numpy.linalg.eig(self.matrix)[0])
+    def _recalculate_weights(self):
+        self._normalized_geometric = normalize_geometric(self.matrix)
+        self._normalized_eigen = normalize_eigen(self.matrix)
+        self._consistency_index, self._consistency_ratio = calculate_consistency(self.matrix, self._normalized_eigen)
 
     @property
     def consistency_index(self):
-        return (self._max_eigenvalue - self.matrix.shape[0]) / (self.matrix.shape[0] - 1)
+        return self._consistency_index
 
     @property
     def consistency_ratio(self):
-        return None
+        return self._consistency_ratio
+
+    def __str__(self):
+        return str(self.matrix)
+
+    def get_consistency_str(self):
+        string_representation = f'{self.name} | ' \
+                                f'CI: {numpy.real_if_close(self.consistency_index):.2f} ' \
+                                f'CR: {numpy.real_if_close(self.consistency_ratio):.2f}\n'
+        if self.has_subcriteria:
+            for subcriteria in self.subcriteria:
+                string_representation += f'  {subcriteria.get_consistency_str()}'
+
+        return string_representation
+
+    def get_ranking_str(self):
+        # round to 2 floating points for each value in numpy arrays
+        geometric_rounded = numpy.round(numpy.real_if_close(self.ranking_geometric), 2)
+        eigen_rounded = numpy.round(numpy.real_if_close(self.ranking_eigen), 2)
+        string_representation = f'{self.name} | Geometric: {geometric_rounded} Eigen: {eigen_rounded}\n'
+        if self.has_subcriteria:
+            for subcriteria in self.subcriteria:
+                string_representation += f'  {subcriteria.get_ranking_str()}'
+
+        return string_representation
--- main.py
+++ main.py
@@ -1,123 +1,18 @@
 import numpy
-from numpy import linalg
-
-
 from comparison_matrix import ComparisonMatrix
 
-random_index = [0, 0, 0.52, 0.89, 1.11, 1.25, 1.35, 1.4, 1.45, 1.49, 1.52]
-
-
-# Compute normalized weights based on Geometric Mean method
-def get_weight_geometric(matrix):
-    column_sum = numpy.tile(numpy.sum(matrix, 0), [matrix.shape[0], 1])
-    normalized_matrix = numpy.divide(matrix, column_sum)
-    geometric_weight = numpy.prod(normalized_matrix, 1) ** (1.0 / matrix.shape[1])
-    return geometric_weight / numpy.sum(geometric_weight)
-
-
-# Compute normalized weights based on Eigen method - return both
-# the maximum eigenvalue and the normalized weight vector
-def get_weight_eigen(matrix):
-    eigenvalues, normalized_eigenvectors = linalg.eig(matrix)
-    max_eigenvalue_index = numpy.argmax(eigenvalues)
-    # where max_eigenvector is the eigenvector corresponding to the maximum eigenvalue
-    max_eigenvector = normalized_eigenvectors[:, max_eigenvalue_index]
-    return eigenvalues[max_eigenvalue_index], max_eigenvector / numpy.sum(max_eigenvector)
-
-
-# compute consistency index and consistency ratio based on
-# outputs from getWeightEigen
-def getCICR(eigL, eigV):
-    # consistency index = (eigL- n) / (n-1)
-    consistency_index = (eigL - eigV.shape[0]) / (eigV.shape[0] - 1)
-    consistency_ratio = consistency_index / random_index[eigV.shape[0]]
-    return consistency_index, consistency_ratio
-
-
-
-
-comparison_matrices = {
-    'PCBake': {
-        'matrix': numpy.array(
-            [[1, 1 / 4, 1 / 3, 1 / 2], [4, 1, 4 / 3, 2], [3, 3 / 4, 1, 6 / 5], [2, 1 / 2, 5 / 6, 1]]),
-        'subcriteria': {
-            'Size': {
-                'matrix': numpy.array([[1, 5], [1 / 5, 1]]),
-            },
-            'Thermal Management': {
-                'matrix': numpy.array([[1, 6], [1 / 6, 1]]),
-            },
-            'Accessibility': {
-                'matrix': numpy.array(
-                    [[1, 6 / 5], [5 / 6, 1]]),
-            },
-            'Cost': {
-                'matrix': numpy.array(
-                    [[1, 2], [1 / 2, 1]]),
-            }
-        }
-    }
-}
-
-
-def calculate_weights(comparison_matrices, indent=0):
-    for criteria in comparison_matrices:
-        normalized_geometric = get_weight_geometric(comparison_matrices[criteria]['matrix'])
-        max_eigenvalue, normalized_eigen = get_weight_eigen(comparison_matrices[criteria]['matrix'])
-        consistency_index, consistency_ratio = getCICR(max_eigenvalue, normalized_eigen)
-
-        comparison_matrices[criteria]['normalized_geometric'] = normalized_geometric
-        comparison_matrices[criteria]['normalized_eigen'] = normalized_eigen
-        comparison_matrices[criteria]['consistency_index'] = consistency_index
-        comparison_matrices[criteria]['consistency_ratio'] = consistency_ratio
-
-        print(
-            f'{"  " * indent}{criteria} CI: {numpy.real_if_close(consistency_index):.2f}, CR: {numpy.real_if_close(consistency_ratio):.2f}')
-
-        if 'subcriteria' in comparison_matrices[criteria]:
-            calculate_weights(comparison_matrices[criteria]['subcriteria'], indent + 1)
-
-        #
-
-
-# returns ranking geometric, eigen
-def calculate_ranking(comparison_matrix):
-    if 'subcriteria' in comparison_matrix:
-        geometric_rankings = []
-        eigen_rankings = []
-        for subcriteria in comparison_matrix['subcriteria']:
-            geometric_ranking, eigen_ranking = calculate_ranking(comparison_matrix['subcriteria'][subcriteria])
-            geometric_rankings.append(geometric_ranking)
-            eigen_rankings.append(eigen_ranking)
-
-        return numpy.matmul(numpy.stack(geometric_rankings, axis=-1),
-                            comparison_matrix['normalized_geometric']), numpy.matmul(
-            numpy.stack(eigen_rankings, axis=-1), comparison_matrix['normalized_eigen'])
-    else:
-        return comparison_matrix['normalized_geometric'], comparison_matrix['normalized_eigen']
-
-
 if __name__ == "__main__":
-    # (1) Calculate the Consistency Index (CI) and Consistency Ratios (CR) of each comparison matrix.
-    print('\n######### 1) CI and CR')
-    print('Calculating Consistency Index (CI) and Consistency Ratios (CR) for each comparison matrix')
-    calculate_weights(comparison_matrices)
+    # create a new comparison matrix
+    subcriterion = [ComparisonMatrix('Size', numpy.array([[1, 5], [1 / 5, 1]])),
+                    ComparisonMatrix('Thermal Management', numpy.array([[1, 6], [1 / 6, 1]])),
+                    ComparisonMatrix('Accessibility', numpy.array([[1, 6 / 5], [5 / 6, 1]])),
+                    ComparisonMatrix('Cost', numpy.array([[1, 2], [1 / 2, 1]]))]
 
-    # (2) Calculate the final rankings of the 4 cars using both the Geometric Mean method and the Eigen
-    # method. Are the two rankings the same?
-    print('\n######### 2) Geometric and Eigen Rankings')
-    geometric_ranking, eigenvalue_ranking = calculate_ranking(comparison_matrices['PCBake'])
-    print(f'Geometric rankings: {numpy.real_if_close(geometric_ranking)}')
-    print(f'Eigenvalue rankings: {numpy.real_if_close(eigenvalue_ranking)}')
+    pcbake = ComparisonMatrix('PCBake', numpy.array(
+        [[1, 1 / 4, 1 / 3, 1 / 2], [4, 1, 4 / 3, 2], [3, 3 / 4, 1, 6 / 5], [2, 1 / 2, 5 / 6, 1]]), subcriterion)
 
-    # (3) A typographical mistake in data entry results in the following criteria level comparison matrix
-    # (the red fonts highlight the mistake). How does the ranking change? Is it possible to spot this
-    # mistake BEFORE computing the ranking?
-    # print('\n######### 3) Typographic Mistake')
-    # comparison_matrices['goal']['matrix'] = numpy.array(
-    #     [[1, 5 / 7, 1 / 5, 5 / 9, 8], [7 / 5, 1, 5 / 9, 5 / 7, 4 / 5], [5, 9 / 5, 1, 7 / 3, 4 / 3],
-    #      [9 / 5, 7 / 5, 3 / 7, 1, 9 / 7], [1 / 8, 5 / 4, 3 / 4, 7 / 9, 1]])
-    # calculate_weights(comparison_matrices)
-    # geometric_ranking, eigenvalue_ranking = calculate_ranking(comparison_matrices['goal'])
-    # print(f'Geometric rankings: {numpy.real_if_close(geometric_ranking)}')
-    # print(f'Eigenvalue rankings: {numpy.real_if_close(eigenvalue_ranking)}')
+    print(pcbake)
+
+    print(pcbake.get_consistency_str())
+
+    print(pcbake.get_ranking_str())
