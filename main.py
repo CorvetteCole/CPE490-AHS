045c5a6bbdb6cbc5099c44f6be3f952fc51fa6e7
Cole Gerdemann <corvettecole@gmail.com>
2023-04-10 10:43:05 -0400

    add thermal system comparison

--- main.py
+++ main.py
@@ -2,61 +2,30 @@ import numpy
 from comparison_matrix import ComparisonMatrix
 
 if __name__ == "__main__":
-    # 	Weights
-    # Cost	0.35
-    # Robustness	0.65
+    # Time Consumption	Open	Closed
+    # Open 	1	2
+    # Closed	(1/2)	1
 
-    # Cost	Hall Effect	Contact	Infrared
-    # Hall Effect	1.00	0.75	0.40
-    # Contact	1.33	1.00	1.50
-    # Infrared	2.50	0.66	1.00
+    time_consumption = ComparisonMatrix("Time Consumption", numpy.matrix(
+        [[1, 2],
+         [0.5, 1]]))
 
-    # Robustness	Hall Effect	Contact	Infrared
-    # Hall Effect	1.00	0.60	0.95
-    # Contact	1.66	1.00	1.50
-    # Infrared	1.05	0.66	1.00
+    # Accuracy	Open	Closed
+    # Open 	1	3
+    # Closed	(1/3)	1
 
-    door_sensor_cost = ComparisonMatrix("Door Sensor Cost", numpy.matrix(
-        [[1, 0.75, 0.40],
-         [1.33, 1, 1.5],
-         [2.5, 0.66, 1]]))
-    door_sensor_robustness = ComparisonMatrix("Door Sensor Robustness", numpy.matrix(
-        [[1, 0.6, 0.95],
-         [1.66, 1, 1.5],
-         [1.05, 0.66, 1]]))
+    accuracy = ComparisonMatrix("Accuracy", numpy.matrix(
+        [[1, 3],
+         [1 / 3, 1]]))
 
-    door_sensor = ComparisonMatrix("Door Sensor", numpy.matrix(
-        [[1, 1.85],
-         [0.53, 1]]), subcriteria=[door_sensor_cost, door_sensor_robustness])
+    # 	Time Consumption	Accuracy
+    # Time Consumption	1	(2/3)
+    # Accuracy	(3/2)	1
 
-    print(door_sensor.get_consistency_str())
-    print(door_sensor.get_ranking_str())
+    control_loop_type = ComparisonMatrix("Control Loop Type", numpy.matrix(
+        [[1, 2 / 3],
+         [3 / 2, 1]]), [time_consumption, accuracy])
 
-    # Temperature Sensor Tables
-    #
-    # Cost	RTD	NTC Glass	NTC Bead
-    # RTD	1.00	2.50	3.00
-    # NTC Glass	0.40	1.00	1.20
-    # NTC Bead	0.33	0.83	1.00
-    #
-    # Robustness	RTD	NTC Glass	NTC Bead
-    # RTD	1.00	0.20	0.30
-    # NTC Glass	5.00	1.00	1.20
-    # NTC Bead	3.33	0.83	1.00
+    print(control_loop_type.get_consistency_str())
 
-    temperature_sensor_cost = ComparisonMatrix("Temperature Sensor Cost", numpy.matrix(
-        [[1, 2.5, 3],
-         [0.4, 1, 1.2],
-         [0.33, 0.83, 1]]))
-
-    temperature_sensor_robustness = ComparisonMatrix("Temperature Sensor Robustness", numpy.matrix(
-        [[1, 0.2, 0.3],
-         [5, 1, 1.2],
-         [3.33, 0.83, 1]]))
-
-    temperature_sensor = ComparisonMatrix("Temperature Sensor", numpy.matrix(
-        [[1, 1.85],
-         [0.53, 1]]), subcriteria=[temperature_sensor_cost, temperature_sensor_robustness])
-
-    print(temperature_sensor.get_consistency_str())
-    print(temperature_sensor.get_ranking_str())
+    print(control_loop_type.get_ranking_str())
