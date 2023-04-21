6b7435ea3f0a38a1a0cb665501b890ec89b55e3c
Cole Gerdemann <corvettecole@gmail.com>
2023-04-21 13:17:01 -0400

    add single dual computer

--- main.py
+++ main.py
@@ -2,61 +2,104 @@ import numpy
 from comparison_matrix import ComparisonMatrix
 
 if __name__ == "__main__":
-    # 	Weights
-    # Cost	0.35
-    # Robustness	0.65
+    # Cost
+    # Single
+    # Separate
+    # Single
+    # 1
+    # 2
+    # Separate
+    # 1/2
+    # 1
+    cost = ComparisonMatrix('Cost', numpy.matrix(
+        [[1, 3 / 4],
+         [4 / 3, 1]]))
 
-    # Cost	Hall Effect	Contact	Infrared
-    # Hall Effect	1.00	0.75	0.40
-    # Contact	1.33	1.00	1.50
-    # Infrared	2.50	0.66	1.00
+    print(cost.get_consistency_str())
+    print(cost.get_ranking_str())
 
-    # Robustness	Hall Effect	Contact	Infrared
-    # Hall Effect	1.00	0.60	0.95
-    # Contact	1.66	1.00	1.50
-    # Infrared	1.05	0.66	1.00
+    # Safety
+    # Single
+    # Separate
+    # Single
+    # 1
+    # 1/3
+    # Separate
+    # 3
+    # 1
 
-    door_sensor_cost = ComparisonMatrix("Door Sensor Cost", numpy.matrix(
-        [[1, 0.75, 0.40],
-         [1.33, 1, 1.5],
-         [2.5, 0.66, 1]]))
-    door_sensor_robustness = ComparisonMatrix("Door Sensor Robustness", numpy.matrix(
-        [[1, 0.6, 0.95],
-         [1.66, 1, 1.5],
-         [1.05, 0.66, 1]]))
+    safety = ComparisonMatrix('Safety', numpy.matrix(
+        [[1, 1 / 3],
+         [3, 1]]))
 
-    door_sensor = ComparisonMatrix("Door Sensor", numpy.matrix(
-        [[1, 1.85],
-         [0.53, 1]]), subcriteria=[door_sensor_cost, door_sensor_robustness])
+    print(safety.get_consistency_str())
+    print(safety.get_ranking_str())
 
-    print(door_sensor.get_consistency_str())
-    print(door_sensor.get_ranking_str())
+    # Performance
+    # Single
+    # Separate
+    # Single
+    # 1
+    # 3/4
+    # Separate
+    # 4/3
+    # 1
 
-    # Temperature Sensor Tables
-    #
-    # Cost	RTD	NTC Glass	NTC Bead
-    # RTD	1.00	2.50	3.00
-    # NTC Glass	0.40	1.00	1.20
-    # NTC Bead	0.33	0.83	1.00
-    #
-    # Robustness	RTD	NTC Glass	NTC Bead
-    # RTD	1.00	0.20	0.30
-    # NTC Glass	5.00	1.00	1.20
-    # NTC Bead	3.33	0.83	1.00
+    performance = ComparisonMatrix('Performance', numpy.matrix(
+        [[1, 3 / 4],
+         [4 / 3, 1]]))
 
-    temperature_sensor_cost = ComparisonMatrix("Temperature Sensor Cost", numpy.matrix(
-        [[1, 2.5, 3],
-         [0.4, 1, 1.2],
-         [0.33, 0.83, 1]]))
+    print(performance.get_consistency_str())
+    print(performance.get_ranking_str())
 
-    temperature_sensor_robustness = ComparisonMatrix("Temperature Sensor Robustness", numpy.matrix(
-        [[1, 0.2, 0.3],
-         [5, 1, 1.2],
-         [3.33, 0.83, 1]]))
+    # Complexity
+    # Single
+    # Separate
+    # Single
+    # 1
+    # 4/3
+    # Separate
+    # 3/4
+    # 1
 
-    temperature_sensor = ComparisonMatrix("Temperature Sensor", numpy.matrix(
-        [[1, 1.85],
-         [0.53, 1]]), subcriteria=[temperature_sensor_cost, temperature_sensor_robustness])
+    complexity = ComparisonMatrix('Complexity', numpy.matrix(
+        [[1, 4 / 3],
+         [3 / 4, 1]]))
 
-    print(temperature_sensor.get_consistency_str())
-    print(temperature_sensor.get_ranking_str())
+    print(complexity.get_consistency_str())
+    print(complexity.get_ranking_str())
+
+    # Criteria
+    # Cost
+    # Safety
+    # Performance
+    # Complexity
+    # Cost
+    # 1
+    # 3
+    # 2
+    # 4
+    # Safety
+    # 1/3
+    # 1
+    # 1/2
+    # 1/2
+    # Performance
+    # 1/2
+    # 2
+    # 1
+    # 3
+    # Complexity
+    # 1/4
+    # 2
+    # 1/3
+    # 1
+
+    criteria = ComparisonMatrix('Criteria', numpy.matrix(
+        [[1, 3, 2, 4],
+         [1 / 3, 1, 1 / 2, 1 / 2],
+         [1 / 2, 2, 1, 3],
+         [1 / 4, 2, 1 / 3, 1]]), [cost, safety, performance, complexity])
+
+    print(criteria.get_consistency_str())
+    print(criteria.get_ranking_str())
